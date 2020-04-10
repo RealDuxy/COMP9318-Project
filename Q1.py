d53b036c1d47eb89f27f965e36bf4c4497a73eb9
Xiyang <du.xi.yang.cool@gmail.com>
2020-04-11 03:54:00 +1000

    加了俩py文件

---
+++ Q1.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    : Q1.py
+@Time    : 2020-04-11 03:50
+@Author  : Duxy
+@Email   : du.xi.yang@qq.com
+@Software: PyCharm
+"""
+
+import helper as h
---
+++ Q2.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    : Q2.py
+@Time    : 2020-04-11 03:50
+@Author  : Chloe
+@Software: PyCharm
+"""
+import helper as h
+
