fc44043d1d40210739779f8a90e4ccfe56bb85a5
Xiyang <du.xi.yang.cool@gmail.com>
2020-04-11 15:43:22 +1000

    更改了Q1的参数错误

--- runImplementation.py
+++ runImplementation.py
@@ -16,7 +16,7 @@ with open('./toy_example/Data_File', 'rb') as f:
 with open('./toy_example/Centroids_File', 'rb') as f:
     Centroids_File = pickle.load(f, encoding = 'bytes')
 start = time.time()
-codebooks, codes = submission.pq(data, P=2, init_centroids=centroids, max_iter = 20)
+codebooks, codes = submission.pq(data=Data_File, P=2, init_centroids=Centroids_File, max_iter = 20)
 end = time.time()
 time_cost_1 = end - start
 
