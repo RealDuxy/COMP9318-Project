1adf529768e281331f2fd326fb7eda9040097b45
Xiyang <du.xi.yang.cool@gmail.com>
2020-04-11 15:58:42 +1000

    添加了centroids data query文件的可视化

--- helper.py
+++ helper.py
@@ -5,6 +5,9 @@
 @Author  : Duxy, Chloe
 @Software: PyCharm
 """
+import submission
+import pickle
+import time
 
 def distance(norm=1):
     '''
@@ -13,3 +16,35 @@ def distance(norm=1):
     '''
     distance = 1
     return distance
+
+def display_centroids():
+    '''
+    将numpy二进制文件 centroids_file 可视化
+    :return: None
+    '''
+    with open('./toy_example/Centroids_File', 'rb') as f:
+        Centroids_File = pickle.load(f, encoding='bytes')
+    print(Centroids_File)
+
+def display_data():
+    '''
+    将numpy二进制文件 data_file 可视化
+    :return: None
+    '''
+    with open('./toy_example/Data_File', 'rb') as f:
+        Data_File = pickle.load(f, encoding='bytes')
+    print(Data_File)
+
+def display_query():
+    '''
+    将numpy二进制文件 query_file 可视化
+    :return: None
+    '''
+    with open('./toy_example/Query_File', 'rb') as f:
+        Query_File = pickle.load(f, encoding='bytes')
+    print(Query_File)
+
+
+display_centroids()
+display_data()
+display_query()
